f56d2a6ce5bdd293efe25e43e0d5f5016825c9e3
Brandon Wang <brandonwang@berkeley.edu>
2012-03-10 05:04:45 -0800

    public pages

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,5 @@
+class ApiController < ApplicationController
+  def index
+    render :layout => 'api'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ CS169Project::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :api
 
   # Sample resource route with options:
   #   resources :products do
