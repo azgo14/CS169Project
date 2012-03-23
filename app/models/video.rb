9ab392851a10afe9e814eb29a7fd039858ac3353
Ben Dong <bdong281@gmail.com>
2012-03-22 17:51:59 -0700

    simple start to video model and spec

---
+++ app/models/video.rb
@@ -0,0 +1,12 @@
+class Video < ActiveRecord::Base
+
+  def upload_to_youtube
+  end
+
+  def make_public
+  end
+
+  def make_private
+  end
+
+end
---
+++ spec/models/video_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Video do
+
+  describe '#upload_to_youtube' do
+    it 'should privately upload the video to youtube' do
+    end
+  end
+
+  describe '#make_public' do
+    it 'should make the video publicly viewable on youtube' do
+    end
+  end
+
+  describe '#make_private' do
+    it 'should make the video not publicly viewable on youtube' do
+    end
+  end
+
+end
