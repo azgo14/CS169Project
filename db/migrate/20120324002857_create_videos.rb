908d0120cd24bf7bdc47b9d663e42d4bdb4626d9
Tiffany Wang <tiffanywang14@berkeley.edu>
2012-03-23 18:11:43 -0700

    videos table

---
+++ db/migrate/20120324002857_create_videos.rb
@@ -0,0 +1,25 @@
+class CreateVideos < ActiveRecord::Migration
+  def up
+    create_table :videos do |t|
+      t.string :youtube_id
+      t.datetime :submission_date
+      t.references :user
+      t.string :name
+      t.string :email
+      t.integer :age
+      #ethnicities?
+      t.string :language
+      t.string :location
+      #additional media?
+      t.text :why #Why did you want to tell this story?
+      t.text :how #How did telling your story change you?
+      t.text :hope #How do you hope hearing your story will change the community?
+      t.text :qa
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :videos
+  end
+end
