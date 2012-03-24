525295995556206139709a08043318956ab876e1
Tiffany Wang <tiffanywang14@berkeley.edu>
2012-03-23 17:17:18 -0700

    some methods implemented, rspec tests passings

--- app/models/video.rb
+++ app/models/video.rb
@@ -9,7 +9,20 @@ class Video < ActiveRecord::Base
   def make_private
   end
 
-  def sort(status)
+  def self.sort(status)
+    Video.find(:all, :order => :submission_date, :conditions => {:status => status})
+  end
+
+  def self.pending_videos
+    sort(:pending)
+  end
+
+  def self.accepted_videos
+    sort(:accepted)
+  end
+
+  def self.rejected_videos
+    sort(:rejected)
   end
 
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -18,25 +18,47 @@ describe Video do
   end
 
   describe '#sort' do
-  before :each do
-    @sorted_videos = [mock('Video'), mock('Video'), mock('Video')]
-  end
-    it 'should sort pending movies by submission date when the argument is :pending' do
-      Video.should_receive(:find).with(:all, :order => :submission_date, :conditions => {:status => :pending}).and_return(@sorted_videos)
+    it 'should sort pending videos by submission date when the argument is :pending' do
+      Video.should_receive(:find).with(:all, :order => :submission_date, :conditions => {:status => :pending})
       Video.sort(:pending)
-      assigns(:pending_videos).should == @sorted_videos
     end
-    
-    it 'should sort accepted movies by submission date when the argument is :accepted' do
-      Video.should_receive(:find).with(:all, :order => :submission_date, :conditions => {:status => :accepted}).and_return(@sorted_videos) 
+
+    it 'should sort accepted videos by submission date when the argument is :accepted' do
+      Video.should_receive(:find).with(:all, :order => :submission_date, :conditions => {:status => :accepted})
       Video.sort(:accepted)
-      assigns(:accepted_videos).should == @sorted_videos
     end
-    
-    it 'should sort rejected movies by submission date when the argument is :rejected' do
-      Video.should_receive(:find).with(:all, :order => :submission_date, :conditions => {:status => :rejected}).and_return(@sorted_videos)
+
+    it 'should sort rejected videos by submission date when the argument is :rejected' do
+      Video.should_receive(:find).with(:all, :order => :submission_date, :conditions => {:status => :rejected})
       Video.sort(:rejected)
-      assigns(:rejected_videos).should == @sorted_videos
+    end
+  end
+
+  describe '#pending_videos, #accepted_videos, #rejected_videos' do
+
+    before :each do
+      @videos = [mock('Video'), mock('Video')]
+    end
+
+    describe '#pending_videos' do
+      it 'should return a collection of pending videos, sorted by submission date' do
+        Video.should_receive(:sort).with(:pending).and_return(@videos)
+        Video.pending_videos
+      end
+    end
+
+    describe '#accepted_videos' do
+      it 'should return a collection of accepted videos, sorted by submission date' do
+        Video.should_receive(:sort).with(:accepted).and_return(@videos)
+        Video.accepted_videos
+      end
+    end
+
+    describe '#rejected_videos' do
+      it 'should return a collection of rejected videos, sorted by submission date' do
+        Video.should_receive(:sort).with(:rejected).and_return(@videos)
+        Video.rejected_videos
+      end
     end
   end
 end
