871d8494c7fe0c0a44c8531560bfe780be8ef2e8
Ben Dong <bdong281@gmail.com>
2012-03-28 21:06:09 -0700

    uploading to youtube works

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -19,6 +19,18 @@ class VideosController < ApplicationController
   end
 
   def create
+    dev_key = 'AI39si6KxFUeYAkWUV5FVWzUThnJGb6PmeSdXOBa9MeKapONiggor24t22qsWmgRGzzWZuaqdQeeMVF8XBaWFDi-_XPgOayKLg'
+    username = 'testacct281'
+    password = 'testpass281'
+    client = YouTubeIt::Client.new(:dev_key => dev_key,
+                                   :username => username,
+                                   :password => password)
+    response = client.video_upload(File.open(params[:video].path), :title => 'test',
+                                   :description => 'desc', :comment => 'denied')
+    video = Video.new(:youtube_id => response.video_id)
+    video.save!
+    flash[:notice] = 'Video uploaded!'
+    redirect_to new_video_path
   end
 
 end
