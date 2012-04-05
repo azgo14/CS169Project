540665d60694b1152e3821809ecf4e741ddc1274
Tiffany Wang <tiffanywang14@berkeley.edu>
2012-04-05 03:29:00 -0700

    admin/videos_controller methods and views done, passes rspec test

--- app/controllers/admin/videos_controller.rb
+++ app/controllers/admin/videos_controller.rb
@@ -1,6 +1,30 @@
 class Admin::VideosController < ApplicationController
 
   def index
+    @pending_videos = Video.pending_videos
+    @accepted_videos = Video.accepted_videos
+    @rejected_videos = Video.rejected_videos
   end
 
+  def show
+    @video = Video.find_by_id(params[:id])
+  end
+
+  def accept
+    @video = Video.find_by_id(params[:id])
+    @video.update_attributes(:status => :accepted)
+    redirect_to admin_videos_path
+  end
+
+  def reject
+    @video = Video.find_by_id(params[:id])
+    @video.update_attributes(:status => :rejected)
+    redirect_to admin_videos_path
+  end
+
+  def pend
+    @video = Video.find_by_id(params[:id])
+    @video.update_attributes(:status => :pending)
+    redirect_to admin_videos_path
+  end
 end
