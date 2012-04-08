fd0d407d60c72bfc748739b212de73fc173cab06
Tiffany Wang <tiffanywang14@berkeley.edu>
2012-04-07 17:54:35 -0700

    more step definitions

--- features/step_definitions/video_steps.rb
+++ features/step_definitions/video_steps.rb
@@ -8,12 +8,10 @@ Given /I am signed in as an administrator/ do
   admin = User.create(:email => 'admin@api.com', :password => 'password')
   admin.admin = true
   admin.save
-  steps %Q{
-    When I am on the user sign-in page
-    And I fill in "user_email" with "admin@api.com"
-    And I fill in "user_password" with "password"
-    Then I press "Sign in"
-  }
+  When %{I am on the user sign-in page}
+  And %{I fill in "user_email" with "admin@api.com"}
+  And %{I fill in "user_password" with "password"}
+  Then %{I press "Sign in"}
 end
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
@@ -23,7 +21,7 @@ end
 
 Then /I should see the following under "(.*)": (.*)/ do |status, video_list|
   video_list.split(/,\s*/).each do |video|
-    step %{Then I should see "#{status}" before "#{video}"}
+    Then %{I should see "#{status}" before "#{video}"}
   end
 end
 
@@ -45,5 +43,11 @@ Then /^(?:|I )should be on (.+)$/ do |page_name|
     current_path.should == path_to(page_name)
   else
     assert_equal path_to(page_name), current_path
+  end
+end
+
+When /I check the following ethnicities: (.*)/ do |ethnicity_list|
+  ethnicity_list.split(/,\s*/).each do |ethnicity|
+    When %{I check "#{ethnicity}"}
   end
 end
