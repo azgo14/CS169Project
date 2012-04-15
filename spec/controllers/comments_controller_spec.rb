fe0cccb50f8a8ba2f4f3a9ad15ce2838f99cb505
Brandon Wang <brandonwang@berkeley.edu>
2012-04-14 23:46:22 -0700

    making more specs for comment controller

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,13 +1,37 @@
 require 'spec_helper'
 
 describe CommentsController do
-  describe '#create' do
-    it 'should allow an anonymous submission and mark the submission as pending' do
-      fake_video = Factory(:video, :youtube_id => '0NwxHphsCxI')
-      Video.should_receive(:find_by_id).with("1234").and_return(fake_video)
-      post :create, {:id => '1234', :content => 'my comment'}
-      response.should redirect_to(show_video_path)
-      Comments.should_receive(:new).with(:status => 'pending')
+  describe '#new' do
+    describe 'submitting an anonymous comment with content' do
+      before(:each) do
+        fake_video = Factory(:video, :youtube_id => '0NwxHphsCxI')
+        Video.should_receive(:find_by_id).with("1234").and_return(fake_video)
+        post :create, {:id => '1234', :content => 'my comment'}
+      end
+      it 'should allow an anonymous submission and mark the submission as pending' do
+        Comments.should_receive(:new).with(:status => 'pending')
+      end
+      it 'should redirect back to the video' do
+        response.should redirect_to(show_video_path)
+      end
+      it 'should store the comment' do
+        fake_video.comments[0].content.should == 'my comment'
+      end
+    end
+
+    describe 'submitting an anonymous comment without content' do
+      before(:each) do
+        fake_video = Factory(:video, :youtube_id => '0NwxHphsCxI')
+        Video.should_receive(:find_by_id).with("1234").and_return(fake_video)
+        post :create, {:id => '1234', :content => 'my comment'}
+      end
+      it 'should not create a new comment' do
+        Comments.should_not_receive(:new)
+      end
+      it 'should flash a notice and redirect back to the video' do
+        response.should redirect_to(show_video_path)
+        flash[:error].should == 'Please fill in the comment before submitting'
+      end
     end
   end
 end
