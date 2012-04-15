68649b604ae6d2505b16d41d2d605ea43b602277
Tiffany Wang <tiffanywang14@berkeley.edu>
2012-04-15 03:37:46 -0700

    admin/comments_controller tests done

--- spec/controllers/admin/comments_controller_spec.rb
+++ spec/controllers/admin/comments_controller_spec.rb
@@ -9,20 +9,98 @@ describe Admin::CommentsController do
 
   describe '#index' do
     it 'should show me a list of comments' do
+      comments = [FactoryGirl.create(:comment), FactoryGirl.create(:comment)]
+      Comment.stub(:pending_comments).and_return(comments)
+      Comment.stub(:accepted_comments).and_return(comments)
+      Comment.stub(:rejected_comments).and_return(comments)
+      get :index
+      assigns(:pending_comments).should == videos
+      assigns(:accepted_comments).should == videos
+      assigns(:rejected_comments).should == videos
     end
   end
 
   describe '#show' do
-    it 'should show the admin details page for the given comment' do
+    it 'should show the comment on the admin details page for the corresponding vide' do
+      comment = FactoryGirl.create(:comment, :id => 1, :video_id => 1)
+      Comment.should_receive(:find_by_id).with('1').and_return(comment)
+      comment.should_receive(:video_id).and_return('1')
+      get :show, :id => 1
+      response.should render_template('videos/show')
+      response.should redirect_to(video_path(comment.video_id) + "comment_#{comment.id}")
     end
   end
 
   describe 'Updating Comment Status' do
+     before :each do
+        @comment = FactoryGirl.create(:comment)
+        Comment.should_receive(:find_by_id).with('1').and_return(@comment)
+      end
+
     describe 'accepting a comment' do
+       after :each do
+        @comment.should_receive(:update_attributes).with(:status => :accepted)
+        @comment.stub(:status).and_return(:accepted)
+        post :accept, :id => 1
+        @comment.status.should == :accepted
+        response.should redirect_to :action => 'index'
+      end
+
+      it 'should allow me to accept a pending comment' do
+        @comment.stub(:status).and_return(:pending)
+      end
+
+      it 'should allow me to accept a rejected video' do
+        @comment.stub(:status).and_return(:rejected)
+      end
+
+      it 'should not affect the comment if I accept an accepted comment' do
+        @comment.stub(:status).and_return(:accepted)
+      end
     end
 
     describe 'rejecting a comment' do
+       after :each do
+        @comment.should_receive(:update_attributes).with(:status => :rejected)
+        @comment.stub(:status).and_return(:rejected)
+        post :accept, :id => 1
+        @comment.status.should == :rejected
+        response.should redirect_to :action => 'index'
+      end
+
+      it 'should allow me to reject a pending comment' do
+        @comment.stub(:status).and_return(:pending)
+      end
+
+      it 'should allow me to reject a rejected video' do
+        @comment.stub(:status).and_return(:accepted)
+      end
+
+      it 'should not affect the comment if I reject a rejected comment' do
+        @comment.stub(:status).and_return(:rejected)
+      end
+    end
+
+    describe 'pending a comment' do
+       after :each do
+        @comment.should_receive(:update_attributes).with(:status => :pending)
+        @comment.stub(:status).and_return(:pending)
+        post :accept, :id => 1
+        @comment.status.should == :pending
+        response.should redirect_to :action => 'index'
+      end
+
+      it 'should allow me to accept a pending comment' do
+        @comment.stub(:status).and_return(:accepted)
+      end
+
+      it 'should allow me to accept a rejected video' do
+        @comment.stub(:status).and_return(:rejected)
+      end
+
+      it 'should not affect the comment if I accept an accepted comment' do
+        @comment.stub(:status).and_return(:pending)
+      end
     end
   end
-
 end
