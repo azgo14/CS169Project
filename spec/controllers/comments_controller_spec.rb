00b33bea82eea0b6cbd03bb25eee639df4d9614f
Brandon Wang <brandonwang@berkeley.edu>
2012-04-14 23:26:22 -0700

    specs with what i think should happen

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe CommentsController do
   describe '#create' do
     it 'should allow an anonymous submission and mark the submission as pending' do
-      pending 'unimplemented'
+      fake_video = Factory(:video, :youtube_id => '0NwxHphsCxI')
+      Video.should_receive(:find_by_id).with("1234").and_return(fake_video)
+      post :create, {:id => '1234', :content => 'my comment'}
+      response.should redirect_to(show_video_path)
+      Comments.should_receive(:new).with(:status => 'pending')
     end
   end
 end
