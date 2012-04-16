fda054ed6c52b35728f435c6b14ed6c0f8a6552b
Tiffany Wang <tiffanywang14@berkeley.edu>
2012-04-15 19:13:56 -0700

    fixed typo

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -5,11 +5,13 @@ describe CommentsController do
   describe 'commenting as an unblocked user' do
 
     before(:each) do
-      @user = FactoryGirl.create(:user)
+      @user = FactoryGirl.create(:user, :id => '1')
       sign_in @user
       fake_video = FactoryGirl.create(:video, :youtube_id => '0NwxHphsCxI')
-      Video.should_receive(:find_by_id).with("1234").and_return(fake_video)
-      post :create, {:id => '1234', :content => 'my comment'}
+      Videos.should_receive(:find_by_id).with("1234").and_return(fake_video)
+      Users.should_receive(:find_by_id).with('1').and_return(@user)
+      @user.should_receive(:blocked).and_return(false)
+      post :create, {:id => '1234', :content => 'my comment', :user_id => '1'}
     end
 
     describe '#new' do
@@ -38,11 +40,13 @@ describe CommentsController do
 
   describe 'commenting as a blocked user'
     before(:each) do
-      @user = FactoryGirl.create(:user, :blocked => true)
+      @user = FactoryGirl.create(:user, :id => '1', :blocked => true)
       sign_in @user
       fake_video = FactoryGirl.create(:video, :youtube_id => '0NwxHphsCxI')
-      Video.should_receive(:find_by_id).with("1234").and_return(fake_video)
-      post :create, {:id => '1234', :content => 'my comment'}
+      Users.should_receive(:find_by_id).with('1').and_return(@user)
+      @user.should_receive(:blocked).and_return(true)
+      Videos.should_receive(:find_by_id).with("1234").and_return(fake_video)
+      post :create, {:id => '1234', :content => 'my comment', :user_id => '1'}
     end
 
     describe '#new' do
@@ -52,7 +56,7 @@ describe CommentsController do
         end
         it 'should flash a notice and redirect back to the video' do
           response.should redirect_to(show_video_path)
-          flash[:error].should == "Sorry, you aren't allowed to post comments'
+          flash[:error].should == "Sorry, you aren't allowed to post comments"
         end
       end
     end
