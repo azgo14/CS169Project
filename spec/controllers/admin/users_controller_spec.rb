7c1480fa92b443bb53ea0ace4d2b5774936c2440
Tiffany Wang <tiffanywang14@berkeley.edu>
2012-04-15 18:55:02 -0700

    tests blocking/unblocking

---
+++ spec/controllers/admin/users_controller_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe UsersController do
+
+  before(:each) do
+    @admin = FactoryGirl.create(:admin)
+    sign_in @admin
+    @user = FactoryGirl.create(:user, :id => 1)
+  end
+
+  describe '#block' do
+    it 'should prevent a user from submitting comments' do
+      Users.should_receive(:find_by_id).with('1').and_return(@user)
+      @user.should_receive(:update_attributes).with(:blocked => true)
+    end
+  end
+
+  describe '#unblock' do
+    it 'should allow a user to submit comments' do
+      Users.should_receive(:find_by_id).with('1').and_return(@user)
+      @user.should_receive(:update_attributes).with(:blocked => false)
+    end
+  end
+end
