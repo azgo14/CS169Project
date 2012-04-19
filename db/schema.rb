a357323d8cf39b4e54a66447ec6e7c73fa1777c1
Brandon Wang <brandonwang@berkeley.edu>
2012-04-18 20:01:37 -0700

    typo fix

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120415061150) do
+ActiveRecord::Schema.define(:version => 20120415232407) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20120415061150) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",                  :default => false
+    t.boolean  "blocked",                :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
