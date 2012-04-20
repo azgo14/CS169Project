b8dec071a27ece8bc5629ba23caf52b93719a294
Tiffany Wang <tiffanywang14@berkeley.edu>
2012-04-19 18:23:06 -0700

    feature file for notes, passing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120419081316) do
+ActiveRecord::Schema.define(:version => 20120419221346) do
 
   create_table "comments", :force => true do |t|
     t.integer  "user_id"
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(:version => 20120419081316) do
     t.string   "title"
     t.string   "about"
     t.integer  "likes"
+    t.text     "notes"
   end
 
 end
