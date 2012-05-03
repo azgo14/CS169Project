a2ce053f9a97a99d9f70384885012627329c4f97
Tiffany Wang <tiffanywang14@berkeley.edu>
2012-05-03 03:13:46 -0700

    can no longer search for and find unaccepted videos, using the url for an unaccepted video redirects

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -18,7 +18,7 @@ class VideosController < ApplicationController
   def show
     id = params[:id]
     @video = Video.find_by_id(id)
-    if not(@video)
+    if not(@video) || @video.status != 'accepted'
       flash[:error] = 'Invalid video id'
       redirect_to videos_path
     end
@@ -77,7 +77,7 @@ class VideosController < ApplicationController
   def like
     id = params[:id]
     @video = Video.find_by_id(id)
-    if not(@video)
+    if not(@video) || @video.status != 'accepted'
       flash[:error] = 'Invalid video id'
       redirect_to videos_path
     end
