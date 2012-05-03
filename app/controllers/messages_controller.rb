0489d2671cacba81ee6357acc8c855ef7928f46a
Brandon Wang <brandonwang@me.com>
2012-05-03 00:55:53 -0700

    barebones viewing messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,7 +5,24 @@ class MessagesController < ApplicationController
       flash[:error] = 'You are not logged in'
       @messages = []
     else
-      @messages = Message.find_all_by_to_user(user.id)
+      @messages = Message.messages_to(user.id)
+    end
+  end
+  def sent_messages
+    user = current_user
+    if user == nil
+      flash[:error] = 'You are not logged in'
+      @messages = []
+    else
+      @messages = Message.messages_from(user.id)
+    end
+  end
+  def show
+    id = params[:id]
+    @message = Message.find_by_id(id)
+    if not(@message.viewable_by(current_user.id))
+      flash[:error] = 'You are not allowed to view this message'
+      redirect_to messages_path
     end
   end
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,42 @@
 class Message < ActiveRecord::Base
+
+  def from_name
+    if from_user == -1
+      return 'Admin'
+    else
+      return User.find_by_id(from_user).email
+    end
+  end
+  
+  def to_name
+    if to_user == -1
+      return 'Admin'
+    else
+      return User.find_by_id(to_user).email
+    end
+  end
+
+  def viewable_by(user_id)
+    if user_id == 'Admin' or user_id == 'admin' or User.find_by_id(user_id).admin
+      return true
+    end
+    if to_user == user_id or from_user == user_id
+      return true
+    end
+  end
+
+  def self.messages_to(user_id)
+    if user_id == 'Admin' or user_id == 'admin' or User.find_by_id(user_id).admin
+      user_id = -1
+    end
+    return Message.find_all_by_to_user(user_id)
+  end
+
+  def self.messages_from(user_id)
+    if user_id == 'Admin' or user_id == 'admin' or User.find_by_id(user_id).admin
+      user_id = -1
+    end
+    return Message.find_all_by_from_user(user_id)
+  end
+
 end
