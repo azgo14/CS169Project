b136ab503d12dedba467b36e0d3b70b15b41b1fc
Tiffany Wang <tiffanywang14@berkeley.edu>
2012-05-03 01:52:29 -0700

    mailer specs passing

--- spec/mailers/submission_mailer_spec.rb
+++ spec/mailers/submission_mailer_spec.rb
@@ -2,27 +2,23 @@ require 'spec_helper'
 
 describe SubmissionMailer do
     
-   before :each do
-    @video = FactoryGirl.create(:video)
-  end
-    
   describe '#submission_accepted' do
     it 'should create a message notifying the recipient that their submission was accepted' do
-      message = SubmissionMailer.submission_accepted(@video)
-      message.from.should == 'info@banyantreeproject.org'
-      message.to.should == @video.email
+      video = FactoryGirl.create(:video)
+      message = SubmissionMailer.submission_accepted(video)
+      message.from.should == ['info@banyantreeproject.org']
+      message.to.should == [video.email]
       message.subject.should == 'Your story for Taking Root has been accepted'
     end
   end
 
   describe '#custom_message' do
     it 'should create a message with a personalized body' do
-      test = "This is a test"
-      subject = "My subject"
-      message = SubmissionMailer.custom_message(@video, subject, text)
-      message.from.should == 'info@banyantreeproject.org'
-      message.to.should == @video.email
-      message.subject.should == "My subject"
+      email = mock('Email', :to => 'email@address.com', :subject => 'My subject', :body => 'My message')
+      message = SubmissionMailer.custom_message(email)
+      message.from.should == ['info@banyantreeproject.org']
+      message.to.should == [email.to]
+      message.subject.should == email.subject
     end
   end
 end
