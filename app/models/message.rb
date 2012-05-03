8f38687e2b87cd6f5f02792325a205dde9c5f1ba
Brandon Wang <brandonwang@me.com>
2012-05-03 02:38:13 -0700

    replying

--- app/models/message.rb
+++ app/models/message.rb
@@ -25,6 +25,28 @@ class Message < ActiveRecord::Base
     end
   end
 
+  def is_collaborator(user_id)
+    if user_id == 'Admin' or user_id == 'admin' or User.find_by_id(user_id).admin
+      user_id = -1
+    end
+    return (user_id == to_user or user_id == from_user)
+  end
+
+  def other_user(user_id)
+    if not is_collaborator(user_id)
+      return nil
+    end
+    if User.find_by_id(user_id).admin
+      user_id = -1
+    end
+    if user_id == to_user
+      return from_user
+    else
+      return to_user
+    end
+  end
+    
+
   def self.messages_to(user_id)
     if user_id == 'Admin' or user_id == 'admin' or User.find_by_id(user_id).admin
       user_id = -1
