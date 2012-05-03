5c4691a06ff383869f4b680b095deb6c928bbbe6
Ben Dong <bdong281@gmail.com>
2012-05-03 02:07:14 -0700

    progress on activity history feature, still need to make more specs/features pass and integrate messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
     if params[:id] == current_user.id.to_s or current_user.admin
       @comments = Comment.find_all_by_user_id(params[:id])
       @videos = Video.find_all_by_user_id(params[:id])
+      @messages = (Message.find_all_by_from_user(params[:id]) |
+                   Message.find_all_by_to_user(params[:id]))
     else
       flash[:error] = 'You are not authorized to view this page'
       redirect_to videos_path
